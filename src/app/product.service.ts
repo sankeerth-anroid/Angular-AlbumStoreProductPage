94f5fa60cbf241ff97e21a30a55eecb5f3539aa3
sankeerth-anroid <64835811+sankeerth-anroid@users.noreply.github.com>
2021-07-23 10:31:52 +0530

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Http,Response } from '@angular/http';
 import { map } from 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
-  _albumUrl = "../assets/album.json";
+  private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
   getAlbum(id: number){
     return this._http.get(this._albumUrl).pipe(map((response: Response) => response.json()));
