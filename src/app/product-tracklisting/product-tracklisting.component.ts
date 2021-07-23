78819e50bf8cd66e700d378633055591172b1de5
sankeerth-anroid <64835811+sankeerth-anroid@users.noreply.github.com>
2021-07-23 11:32:50 +0530

    update product-tracklist component

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 import {Album} from '../album'
 @Component({
   selector: 'app-product-tracklisting',
