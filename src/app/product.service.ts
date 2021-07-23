431bf317baf05d1d28c62865b8688279c369954f
sankeerth-anroid <64835811+sankeerth-anroid@users.noreply.github.com>
2021-07-23 10:28:55 +0530

    Product-description page

---
+++ src/app/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { map } from 'rxjs/add/operator/map';
+@Injectable()
+export class ProductService {
+  _albumUrl = "../assets/album.json";
+  constructor(private _http: Http) { }
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).pipe(map((response: Response) => response.json()));
+  }
+}
